5ec6080a29d39026faa0b65d4f96bc4ca3d1f89f
Matt <MattAG@users.noreply.github.com>
2020-11-28 19:07:39 +0000

    Added input model for related tags

---
+++ Website.Services/Inputs/PostTagInput.cs
@@ -0,0 +1,10 @@
+﻿using HotChocolate.Types.Relay;
+using Website.Data.Entities;
+
+namespace Website.Services.Inputs
+{
+    /// <summary>
+    /// Input model for the related tags, when adding or updating a post.
+    /// </summary>
+    public record PostTagInput([ID(nameof(Tag))] int Id);
+}
